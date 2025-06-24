229b31218557f796bcee7d0b4032563294468664
Jose Jimenez <josejimenez2003w@gmail.com>
2025-06-24 14:54:50 -0600

    Limpiar rama para iniciar nuevo proyecto

--- backend/models/Usuario.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UsuarioSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  correo: { type: String, required: true, unique: true },
-  contrasena: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Usuario', UsuarioSchema);
--- backend/routes/usuarios.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registrarUsuario, loginUsuario } = require('../controllers/usuariosController');
-
-router.post('/registro', registrarUsuario);
-router.post('/login', loginUsuario);
-
-module.exports = router;
